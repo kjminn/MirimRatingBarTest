b7c800eaa206ca2e74cb77e2f64e02570564ebe6
kjminn <kjminn2001@gmail.com>
2020-11-23 19:37:33 +0900

    Complete RatingBar Test

--- app/src/main/java/kr/ac/ssu/kjminn/mirimratingbartest/MainActivity.java
+++ app/src/main/java/kr/ac/ssu/kjminn/mirimratingbartest/MainActivity.java
@@ -3,12 +3,41 @@ package kr.ac.ssu.kjminn.mirimratingbartest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar1, ratingBar2, ratingBar3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratingBar1 = findViewById(R.id.rating_bar1);
+        ratingBar2 = findViewById(R.id.rating_bar2);
+        ratingBar3 = findViewById(R.id.rating_bar3);
+        Button btnIncr = findViewById(R.id.btn_incr);
+        Button btnDecr = findViewById(R.id.btn_decr);
+        btnIncr.setOnClickListener(btnListener);
+        btnDecr.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_incr:
+                    ratingBar1.setRating(ratingBar1.getRating() + ratingBar1.getStepSize());
+                    ratingBar2.setRating(ratingBar2.getRating() + ratingBar2.getStepSize());
+                    ratingBar3.setRating(ratingBar3.getRating() + ratingBar3.getStepSize());
+                    break;
+                case R.id.btn_decr:
+                    ratingBar1.setRating(ratingBar1.getRating() - ratingBar1.getStepSize());
+                    ratingBar2.setRating(ratingBar2.getRating() - ratingBar2.getStepSize());
+                    ratingBar3.setRating(ratingBar3.getRating() - ratingBar3.getStepSize());
+                    break;
+            }
+        }
+    };
 }
